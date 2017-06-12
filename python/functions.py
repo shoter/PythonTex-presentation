ae91dc8e9ab3660abfb35c601b8737cdca92f727
Damian <lubiebardzozelki@gmail.com>
2017-06-12 09:30:51 +0200

    Added first version of file

---
+++ python/functions.py
@@ -0,0 +1,4 @@
+def sayMyName(name):
+    print ("Your name is {0}".format(name))
+sayMyName("Damian")
+
---
+++ python/indentation.py
@@ -0,0 +1,7 @@
+# And this is a comment.
+from random import randint
+number = randint(0, 9)
+if number < 5:
+    print("0-4")
+else:
+    print("5-9")
